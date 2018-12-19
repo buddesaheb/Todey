c8b5cc97b9c4cc2d99eccdf30fb980f047ce1b92
Kabuli Behera <kbehera1@in.ibm.com>
2018-12-19 17:16:28 +0530

    data into codable using plist

--- Todey/Controllers/TodoListViewController.swift
+++ Todey/Controllers/TodoListViewController.swift
@@ -11,29 +11,15 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
-    var defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
         
-        let newItem2 = Item()
-        newItem2.title = "Bring Milk"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Destory Demogonous"
-        itemArray.append(newItem3)
-        
-                if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-                    itemArray = items
-                }
-        
-        
-        // Do any additional setup after loading the view, typically from a nib.
+        loadItems()
     }
     //    Mark: Tableview DataSource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -45,12 +31,12 @@ class TodoListViewController: UITableViewController {
         let item = itemArray[indexPath.row]
         
         cell.textLabel?.text = item.title
-//        if item.done == true {
-//            cell.accessoryType = .checkmark
-//        }else {
-//            cell.accessoryType = .none
-//        }
-//        This above experision is converted like Ternary operator
+        //        if item.done == true {
+        //            cell.accessoryType = .checkmark
+        //        }else {
+        //            cell.accessoryType = .none
+        //        }
+        //        This above experision is converted like Ternary operator
         
         cell.accessoryType = item.done ? .checkmark : .none
         return cell
@@ -58,6 +44,7 @@ class TodoListViewController: UITableViewController {
     //    Mark: Tableview Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItem()
         
         
         tableView.reloadData()
@@ -74,8 +61,9 @@ class TodoListViewController: UITableViewController {
             let newItem = Item()
             newItem.title = textFeild.text!
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
+            self.saveItem()
+            
+            
             
             
         }
@@ -85,6 +73,29 @@ class TodoListViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert,animated: true, completion: nil)
+    }
+    func saveItem(){
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array!")
+        }
+        
+        self.tableView.reloadData()
+    }
+    func loadItems(){
+        if let data = try?  Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }catch{
+                print(error)
+            }
+        }
+        
     }
 }
 
